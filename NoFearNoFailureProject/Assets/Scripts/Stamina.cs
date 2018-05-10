15626809d56d5e716165d0f31b9b3fac83d5369b
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-05-10 10:27:07 -0700

    Fixed Enemy Stuff, Added Potions and other things -JT

---
+++ NoFearNoFailureProject/Assets/Scripts/Stamina.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stamina : MonoBehaviour {
+    public GameObject StaminaText;
+    public int stamina = 100;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        StaminaText.GetComponent<Text>().text = ("Stamina: " + stamina);
+        if (stamina > 100)
+        {
+            stamina = 100;
+        }
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            stamina = stamina + 5;
+            Destroy(gameObject);
+        }
+    }
+}
