4347c57c7620ac79087147ff8f05c586518d029f
Alexander Petty <18.alexander.petty@ksd.org>
2018-05-11 09:48:11 -0700

    Death my Cubes

---
+++ NoFearNoFailureProject/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public float timer = 0.0f;
+	public float spawntimer = 2.0f;
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        Vector3 spawnPosition = transform.position;
+		
+		if (timer >= spawntimer) {
+
+			GameObject enemy = (GameObject)Instantiate (prefab, spawnPosition, Quaternion.identity);
+            timer = 0;
+		}
+	}
+}
